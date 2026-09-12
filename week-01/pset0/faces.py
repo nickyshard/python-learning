7656c7edddf2ae1ea28c9349f4f900ff26c488dc
nickyshard <nikishardlow@gmail.com>
2026-09-12 23:00:14 +0200

    Add pset0 faces exercise

---
+++ week-01/pset0/faces.py
@@ -0,0 +1,11 @@
+def main():
+    text = input()
+    text = convert(text)
+    print(text)
+    
+
+def convert(inputtext):
+    outputtext = inputtext.replace(":)","🙂").replace(":(","🙁")
+    return outputtext
+
+main()
